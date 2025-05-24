d2ea8109bba3fa156505fcf173b5f8115313b3bd
mikedevv <maungdevv@gmail.com>
2025-05-24 14:26:00 +0630

    first commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['www.livescience.com', 'nasawatch.com', 'nypost.com', 'www.earth.com', 'farmingdale-observer.com', 'www.space.com', 'phys.org', 'indiandefencereview.com', 'www.cnn.com', 'cff2.earth.com', 'images.pexels.com', 'cdn.mos.cms.futurecdn.net', 'scx2.b-cdn.net', 'assets.iflscience.com',],
+  }
 };
 
 export default nextConfig;
