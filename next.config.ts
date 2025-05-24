2edcbb3e224fca4cdef111eeab225fa98a11cc0c
mikedevv <maungdevv@gmail.com>
2025-05-24 17:40:29 +0630

    feat: logo added

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['www.livescience.com', 'nasawatch.com', 'nypost.com', 'www.earth.com', 'farmingdale-observer.com', 'www.space.com', 'phys.org', 'indiandefencereview.com', 'www.cnn.com', 'cff2.earth.com', 'images.pexels.com', 'cdn.mos.cms.futurecdn.net', 'scx2.b-cdn.net', 'assets.iflscience.com',],
+    domains: ['www.livescience.com', 'nasawatch.com', 'nypost.com', 'www.earth.com', 'farmingdale-observer.com', 'www.space.com', 'phys.org', 'indiandefencereview.com', 'www.cnn.com', 'cff2.earth.com', 'images.pexels.com', 'cdn.mos.cms.futurecdn.net', 'scx2.b-cdn.net', 'assets.iflscience.com', 'media.cnn.com', 'www.washingtonpost.com'],
   }
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 import Link from "next/link";
+import BlogList from "@/components/blog-list";
 type PostType = {
   title: string
   description: string
@@ -28,42 +29,9 @@ export default async function Home() {
   return (
     <div className="grid grid-rows-[auto_1fr_auto] min-h-screen p-8 sm:p-16 font-sans bg-white text-black">
       <main className="w-full flex flex-col items-center gap-12">
+        <Image width={100} height={100} src="/logo.svg" alt="logo" />
         <h1 className="text-3xl font-bold text-center">Notes from Someone</h1>
-        <div className="blog-list grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 w-full">
-          {posts.articles?.slice(0, 10).map((post: PostType, index) => (
-            <Card key={index} className="w-full max-w-sm mx-auto shadow-md rounded-lg overflow-hidden">
-              <CardHeader className="p-4">
-                <CardTitle className="text-base font-semibold mb-1">
-                  {post.title}
-                </CardTitle>
-                <span className="text-xs text-gray-500">{new Date(post.publishedAt).toLocaleDateString()}</span>
-                <div className="mt-3">
-                  <Image
-                    className="w-full h-48 object-cover rounded"
-                    width={500}
-                    height={300}
-                    src={
-                      post?.urlToImage ||
-                      "https://images.pexels.com/photos/32058694/pexels-photo-32058694/free-photo-of-cozy-alpine-cabins-in-misty-mountain-landscape.jpeg?auto=compress&cs=tinysrgb&w=800"
-                    }
-                    alt={post.title}
-                  />
-                </div>
-              </CardHeader>
-
-              <CardContent className="p-4">
-                <p className="text-sm text-gray-700 line-clamp-3">{post.description}</p>
-              </CardContent>
-              <CardContent>
-                <Link href={post?.url} className="text-xs text-blue-400">See more...</Link>
-              </CardContent>
-
-              <CardFooter className="p-4 pt-0">
-                <p className="text-xs text-gray-600">Author - {post.author || 'unknown'}</p>
-              </CardFooter>
-            </Card>
-          ))}
-        </div>
+        <BlogList />
       </main>
 
       <footer className="row-start-3 mt-16 text-sm text-center text-gray-500">
